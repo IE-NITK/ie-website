f59c3ee698eac90f53922ca6d518ff91f032bb65
Mishal Shah <shahmishal1998@gmail.com>
2018-12-23 22:59:49 +0530

    new logo, added favicon icon, some 404 fixes

--- sig/urls.py
+++ sig/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+
 from sig import views
 
 app_name = 'sig'
 
 urlpatterns = [
 	url(r'^sig/$', views.view_sig, name='sig')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
