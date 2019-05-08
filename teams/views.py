f47d06d234c128c28ce29c455f2ea2cdd0f71191
devil-r <dtushar279@gmail.com>
2019-05-09 01:28:04 +0530

    spark and publicity team and twitter account

--- teams/views.py
+++ teams/views.py
@@ -10,5 +10,7 @@ def view_teams(request):
     gadget = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'gadget.json')).read())
     garage = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'garage.json')).read())
     website = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'website.json')).read())
-    context={'core': core,'code': code, 'gadget': gadget, 'garage': garage, 'website': website}
+    spark = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'spark.json')).read())
+    publicity = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'publicity.json')).read())
+    context={'core': core,'code': code, 'gadget': gadget, 'garage': garage, 'website': website ,'spark' :spark,'publicity':publicity}
     return render(request, 'teams.html',context)
