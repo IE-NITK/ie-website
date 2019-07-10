129a872248373d866105cf9e1408dda6a18eee8b
Chaitany Pandiya <32352441+chaitany10@users.noreply.github.com>
2019-07-10 20:23:18 +0530

    Update views_home.py

--- accounts/views_home.py
+++ accounts/views_home.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 
