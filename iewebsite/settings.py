90701b297d317429986c4616a8a46b18869609ef
chaitany10 <chaitany.pandiya@gmail.com>
2019-09-03 23:01:46 +0530

    Added embeded gform and marquee for enigma19

--- iewebsite/settings.py
+++ iewebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!th-(6rochf+vi!$e(o84^-)3+&oj7osgrztitz5p#8_mtq5t%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # DEBUG = True	#for localhost
 
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'iewebsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iewebsite',
-        'USER': 'iewebsite',
-        'PASSWORD': 'mnsm',
-        'HOST': 'localhost',
-        'PORT': '',
-        }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'iewebsite',
+    #     'USER': 'iewebsite',
+    #     'PASSWORD': 'mnsm',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    #     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
