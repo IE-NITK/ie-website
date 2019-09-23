5e9612a14593492e78914b0900c385255fda8285
madhumithanara <madhumithanara.171it125@nitk.edu.in>
2019-09-23 22:55:40 +0530

    Events update

--- iewebsite/settings.py
+++ iewebsite/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
         }
-     # 'default': {
-     #    'ENGINE': 'django.db.backends.sqlite3',
-     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
