700744b1998c736fdcd487c65d0e52e783448f79
Chaitany Pandiya <chaitany.pandiya@gmail.com>
2020-07-26 21:20:02 +0530

    added virtual expo

--- virtual_expo/urls.py
+++ virtual_expo/urls.py
@@ -9,7 +9,14 @@ from django.conf.urls.static import static
 app_name = 'virtual_expo'
 
 urlpatterns = [
-    path('virtual-expo/crowd-counting', views.crowd_counting, name='events'),
-    
+    path('virtual-expo/', views.virtual_expo, name='virtual_expo'),
+    path('virtual-expo/code/crowd-counting', views.crowd_counting, name='crowd-counting'),
+    path('virtual-expo/code/indian-sign-lang-interpretor', views.indian_sign_lang_interpretor, name='indian-sign-lang-interpretor'),
+    path('virtual-expo/code/image-captioning', views.image_captioning, name='image_captioning'),
+    path('virtual-expo/code/mood_based_movie_recommender', views.mood_based_movie_recommender, name='mood_based_movie_recommender'),
+    path('virtual-expo/code/navigation_using_RL', views.navigation_using_RL, name='navigation_using_RL'),
+    path('virtual-expo/code/semantic_segmentation', views.semantic_segmentation, name='semantic_segmentation'),
+    path('virtual-expo/code/toll_cost_calculator', views.toll_cost_calculator, name='toll_cost_calculator'),
+    path('virtual-expo/code/ndn', views.ndn, name='NDN'),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- virtual_expo/views.py
+++ virtual_expo/views.py
@@ -2,17 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def virtual_expo(request):
+    return render(request, 'coming-soon.html')
+
 def crowd_counting(request):
     return render()
 
 def indian_sign_lang_interpretor(request):
+    return render()
 
-def image_captioning():
+def image_captioning(request):
+    return render()
 
-def mood_based_movie_recommender():
+def mood_based_movie_recommender(request):
+    return render()
 
-def navigation_using_RL():
+def navigation_using_RL(request):
+    return render()
 
-def semantic_segmentation():
+def semantic_segmentation(request):
+    return render()
 
-def toll_cost_calculator():
+def toll_cost_calculator(request):
+    return render()
+
+def ndn(request):
+    return render()
