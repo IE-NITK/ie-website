36cc488b7eb542f19b11c35557875ebe679864ab
Chaitany Pandiya <chaitany.pandiya@gmail.com>
2020-07-27 02:59:44 +0530

    Added Code Projects for virtual expo

--- virtual_expo/views.py
+++ virtual_expo/views.py
@@ -3,28 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def virtual_expo(request):
-    return render(request, 'coming-soon.html')
+    return render(request, 'virtual_expo.html')
 
 def crowd_counting(request):
-    return render()
+    return render(request, 'crowd_counting.html')
 
 def indian_sign_lang_interpretor(request):
-    return render()
+    return render(request, 'indian-sign-lang-interpretor.html')
 
 def image_captioning(request):
-    return render()
+    return render(request, 'image-captioning.html')
 
 def mood_based_movie_recommender(request):
-    return render()
+    return render(request, 'mood-based-movie-recommender.html')
 
 def navigation_using_RL(request):
-    return render()
+    return render(request,'navigation-using-RL.html')
 
 def semantic_segmentation(request):
-    return render()
+    return render(request,'semantic-segmentation.html')
 
 def toll_cost_calculator(request):
-    return render()
+    return render(request,'toll-cost-calculator.html')
 
 def ndn(request):
-    return render()
+    return render(request,'ndn.html')
