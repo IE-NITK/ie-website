6be1ef473ba5b6b2c35bbe9ce26b9928f7aca6d9
Chaitany Pandiya <chaitany.pandiya@gmail.com>
2020-08-13 00:04:53 +0530

    Added admin list oprtions for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,9 @@ from accounts.models import Profile, Account, Status, RoundOneSubmission, Activa
 
 
 admin.site.register(Profile)
-admin.site.register(Account)
+@admin.register(Account)
+class itemAdmin(admin.ModelAdmin):
+    list_display = ('role', 'profile', 'user', 'is_active', 'archive', 'SIG', 'roll_no')
 admin.site.register(Status)
 admin.site.register(RoundOneSubmission)
 admin.site.register(ActivationRecord)
