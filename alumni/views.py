f7a0cfe685a9ba1ef5d2d5353553ab04e30cf1a2
PiyushPI07 <ingalepiyush459@gmail.com>
2020-10-05 02:12:07 +0530

    Updated Alumni 2020

--- alumni/views.py
+++ alumni/views.py
@@ -5,11 +5,12 @@ from django.conf import settings
 
 
 def view_alumni(request):
+    alumni2020 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2020.json')).read())
     alumni2019 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2019.json')).read())
     alumni2018 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2018.json')).read())
     alumni2017 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2017.json')).read())
     alumni2016 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2016.json')).read())
     alumni2015 = json.loads(open(os.path.join(settings.STATICFILES_DIRS[0], 'alumni2015.json')).read())
 
-    context={'alumni2019': alumni2019,'alumni2018': alumni2018, 'alumni2017': alumni2017, 'alumni2016': alumni2016, 'alumni2015': alumni2015}
+    context={'alumni2020': alumni2020,'alumni2019': alumni2019,'alumni2018': alumni2018, 'alumni2017': alumni2017, 'alumni2016': alumni2016, 'alumni2015': alumni2015}
     return render(request, 'alumni.html',context)
