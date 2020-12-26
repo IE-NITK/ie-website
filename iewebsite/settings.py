223598331cfd52950309a5c73efbf23886575ae4
Paranjaysaxena <42527319+Paranjaysaxena@users.noreply.github.com>
2020-12-26 22:20:36 +0530

    add tectonic

--- iewebsite/settings.py
+++ iewebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!th-(6rochf+vi!$e(o84^-)3+&oj7osgrztitz5p#8_mtq5t%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+DEBUG = False
 
-DEBUG = True  # for localhost
+# DEBUG = True  # for localhost
 
 ALLOWED_HOSTS = ['ie.nitk.ac.in', '10.4.0.67', '*']
 
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'iewebsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'iewebsite',
-    #     'USER': 'iewebsite',
-    #     'PASSWORD': 'mnsm',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    #     }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iewebsite',
+        'USER': 'iewebsite',
+        'PASSWORD': 'mnsm',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
