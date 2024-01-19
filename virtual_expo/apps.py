b227679ea55737d371f1c2ce98c23b6779145b73
Dibyam Kumar <dibyam1101@gmail.com>
2024-01-20 00:16:58 +0530

    Clean Repo

--- virtual_expo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VirtualExpoConfig(AppConfig):
-    name = 'virtual_expo'
--- webadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebadminConfig(AppConfig):
-    name = 'webadmin'
